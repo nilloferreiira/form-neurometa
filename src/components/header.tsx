6361f02d07e12fab2d1b443e74ef321bb5ef501c
Danillo <danillodana@outlook.com>
2024-04-25 17:34:54 -0300

    form without select fields

---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+  return (
+    <header className="w-4/5 mx-auto p-2">
+      <h1>Preencha os campos abaixo para fazer seu cadastro</h1>
+      <div className="w-full bg-white h-[1px]" />
+    </header>
+  );
+}
