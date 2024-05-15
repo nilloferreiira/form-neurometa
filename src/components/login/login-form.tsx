f72a243487b14fce541b8c2b4db8480c070fa892
Danillo <danillodana@outlook.com>
2024-05-14 22:19:18 -0300

    show password

--- src/components/login/login-form.tsx
+++ src/components/login/login-form.tsx
@@ -9,6 +9,9 @@ import { LoginSchema, loginSchema } from "@/utils/login-schema";
 import { useLogin } from "@/hooks/use-login";
 import { toast } from "../ui/use-toast";
 import {useRouter} from "next/navigation";
+import { useState } from "react";
+import { Checkbox } from "../ui/checkbox";
+import { Label } from "../ui/label";
 
 export function LoginForm() {
   const {
@@ -20,6 +23,8 @@ export function LoginForm() {
   });
 
   const router = useRouter()
+
+  const [showPassword, setShowPassword] = useState(false)
 
   const onSubmit: SubmitHandler<LoginSchema> = async (data) => {
     const { handleLogin } = useLogin();
@@ -45,7 +50,10 @@ export function LoginForm() {
     >
       <Input type="email" placeholder="email" {...register("email")} />
       {errors.email && <ErrorSpan>{errors.email.message}</ErrorSpan>}
-      <Input type="text" placeholder="senha" {...register("password")} />
+      <Input type={showPassword ? 'text': 'password'} placeholder="senha" {...register("password")} />
+      <div className="flex gap-2 items-center">
+      <Checkbox id="showPassword" onCheckedChange={() => setShowPassword(!showPassword)} /> <Label htmlFor="showPassword">Mostrar senha</Label>
+      </div>
       {errors.password && <ErrorSpan>{errors.password.message}</ErrorSpan>}
       <Button disabled={isSubmitting} className="bg-royleBlue hover:bg-royleBlue/80 text-zinc-50 p-2 text-sm md:text-base rounded-md font-regular">
         {isSubmitting ? 'Logando' : 'Login'}
