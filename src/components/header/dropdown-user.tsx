ef4fd3f33f5ae98cd2091134d0ce7a8f67b4cc0c
Danillo <danillodana@outlook.com>
2024-05-17 12:40:57 -0300

    profile picture

--- src/components/header/dropdown-user.tsx
+++ src/components/header/dropdown-user.tsx
@@ -10,6 +10,7 @@ import { ChevronDown } from "lucide-react";
 import { LogoutBtn } from "./logout-btn";
 import { useEffect, useState } from "react";
 import { User, useUser } from "@/hooks/use-user";
+import Image from "next/image";
 
 export function DropDownUser() {
   const [user, setUser] = useState<User | null>();
@@ -30,9 +31,20 @@ export function DropDownUser() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="flex gap-2 border-[1px] border-royleBlue/20 rounded-lg p-2 outline-none">
-        {user !== null && user?.nome} <ChevronDown />
+        {user?.fotoPerfil ? (
+          <div className="flex items-center justify-center">
+          <Image src={user.fotoPerfil} width={40} height={40} alt="Foto de perfil" className="w-10 h-10 rounded-full" />  <ChevronDown />
+          </div>
+        ) : (
+          <>
+            {user?.nome} <ChevronDown />
+          </>
+        )}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
+        <DropdownMenuItem>
+          {user !== null && user?.nome}
+        </DropdownMenuItem>
         <DropdownMenuItem>
           <LogoutBtn />
         </DropdownMenuItem>
