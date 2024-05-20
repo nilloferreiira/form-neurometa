43ebe4fde55c9e0ef941517eb4f8e8dc58985954
Danillo <danillodana@outlook.com>
2024-05-20 16:08:39 -0300

    new uf select component and warning login toast

--- src/components/form/select-uf.tsx
+++ src/components/form/select-uf.tsx
@@ -1,90 +1,43 @@
-import { Separator } from "@radix-ui/react-select";
 import { SelectContent, SelectItem } from "../ui/select";
 
 export function SelectUF() {
+  const ufArray = [
+    { value: "AC", label: "AC" },
+    { value: "AL", label: "AL" },
+    { value: "AM", label: "AM" },
+    { value: "AP", label: "AP" },
+    { value: "BA", label: "BA" },
+    { value: "CE", label: "CE" },
+    { value: "DF", label: "DF" },
+    { value: "ES", label: "ES" },
+    { value: "GO", label: "GO" },
+    { value: "MA", label: "MA" },
+    { value: "MG", label: "MG" },
+    { value: "MS", label: "MS" },
+    { value: "MT", label: "MT" },
+    { value: "PA", label: "PA" },
+    { value: "PB", label: "PB" },
+    { value: "PE", label: "PE" },
+    { value: "PI", label: "PI" },
+    { value: "PR", label: "PR" },
+    { value: "RJ", label: "RJ" },
+    { value: "RN", label: "RN" },
+    { value: "RO", label: "RO" },
+    { value: "RR", label: "RR" },
+    { value: "RS", label: "RS" },
+    { value: "SC", label: "SC" },
+    { value: "SE", label: "SE" },
+    { value: "SP", label: "SP" },
+    { value: "TO", label: "TO" },
+  ];
+
   return (
     <SelectContent className="bg-royleBlue/20">
-      <SelectItem className="bg-zinc-50" value="AC">
-        AC
+      {ufArray.map((uf) => (
+      <SelectItem key={uf.value} className="bg-zinc-50" value={uf.value}>
+        {uf.label}
       </SelectItem>
-      <SelectItem className="bg-zinc-50" value="AL">
-        AL
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="AM">
-        AM
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="AP">
-        AP
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="BA">
-        BA
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="CE">
-        CE
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="DF">
-        DF
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="ES">
-        ES
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="GO">
-        GO
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="MA">
-        MA
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="MG">
-        MG
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="MS">
-        MS
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="MT">
-        MT
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="PA">
-        PA
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="PB">
-        PB
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="PE">
-        PE
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="PI">
-        PI
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="PR">
-        PR
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="RJ">
-        RJ
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="RN">
-        RN
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="RO">
-        RO
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="RR">
-        RR
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="RS">
-        RS
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="SC">
-        SC
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="SE">
-        SE
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="SP">
-        SP
-      </SelectItem>
-      <SelectItem className="bg-zinc-50" value="TO">
-        TO
-      </SelectItem>
+      ))}
     </SelectContent>
   );
 }
