edf27d9e8b1f3a21456ca4f42995f20cd1f2d49c
Jose Wellington <josewellington@infowsoftware.com.br>
2024-05-22 14:04:49 -0300

    imagem aparecendo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export function Logo() {
-    return (
-        <div className="size-12 rounded-md overflow-hidden">
-        <Image
-          src={"/neurometa.jpeg"}
-          alt={"Neurometa"}
-          width={200}
-          height={200}
-        />
-      </div>
-    )
+  return (
+    <div className="size-12 rounded-md overflow-hidden">
+      <Image
+        src="/assets/neurometa.jpeg" // path relative to the public folder
+        alt="Neurometa"
+        width={200}
+        height={200}
+      />
+    </div>
+  );
 }
